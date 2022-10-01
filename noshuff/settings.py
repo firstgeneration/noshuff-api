8fb5f9995b5f9a86d6158dc68cee8df2fa189a12
Daniel Gantz <daniel.gantz@gmail.com>
2022-10-01 18:43:51 +0300

    Env variables

--- noshuff/settings.py
+++ noshuff/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for noshuff project.
 
@@ -11,19 +12,29 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gonna_be_an_env_in_the_next_commit_:P'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
