bc736c5470508317f90c59d91d92963b3ba36b99
Daniel Gantz <daniel.gantz@gmail.com>
2022-10-03 22:23:24 +0300

    Configure formatting packages

--- noshuff/settings.py
+++ noshuff/settings.py
@@ -11,9 +11,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
-import environ
 import os
+from pathlib import Path
+
+import environ
 
 env = environ.Env(
     # set casting, default value
