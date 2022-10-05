af7dbfb30de50b6f9f8da9445d840e2d9659d3d2
Daniel Gantz <daniel.gantz@gmail.com>
2022-10-05 20:45:23 +0300

    Create User model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+
+class User(TimeStampedModel, AbstractUser):
+    email = models.EmailField(unique=True)
+    spotify_id = models.CharField(max_length=100)
+    spotify_display_name = models.CharField(max_length=100)
+    spotify_avatar_url = models.URLField(null=True)
+    spotify_access_token = models.CharField(max_length=100, null=True, blank=True)
+    spotify_refresh_token = models.CharField(max_length=100, null=True, blank=True)
