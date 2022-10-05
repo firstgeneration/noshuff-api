5c04df34d6546de84b11a242b95f5cf14369c0e5
Daniel Gantz <daniel.gantz@gmail.com>
2022-10-05 21:40:03 +0300

    Initial User migration

--- core/models.py
+++ core/models.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
+import uuid
 
 
 class User(TimeStampedModel, AbstractUser):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     email = models.EmailField(unique=True)
     spotify_id = models.CharField(max_length=100)
     spotify_display_name = models.CharField(max_length=100)
