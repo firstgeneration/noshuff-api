98096418b837cd30c1d54e06f0374c03e8ac6416
Daniel Gantz <daniel.gantz@gmail.com>
2024-10-16 21:35:39 +0300

    Set up routes and views for OAuth2

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def spotify_pre_auth(request):
+    pass
+
+def spotify_post_auth_callback(request):
+    pass
+
+# TODO: Remove this when frontend is built
+def frontend_placeholder_redirect(request):
+    pass
--- noshuff_api/urls.py
+++ noshuff_api/urls.py
@@ -1,22 +1,18 @@
-"""
-URL configuration for noshuff_api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # Authentication
+    path('auth', views.spotify_pre_auth, name='spotify_pre_auth'),
+    path('post-auth', views.spotify_post_auth_callback, name='spotify_post_auth_callback'),
+    # TODO: Remove this when frontend is built
+    path(
+        'frontend-placeholder-redirect',
+        views.frontend_placeholder_redirect,
+        name='frontend_placeholder_redirect'
+    ),
 ]
