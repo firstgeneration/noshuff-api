ad941ff0dd8d6e7921462db886f67578601030e6
Daniel Gantz <daniel.gantz@gmail.com>
2024-10-27 16:19:30 +0200

    Update user model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 class TimestampModelMixin(models.Model):
@@ -10,7 +10,28 @@ class TimestampModelMixin(models.Model):
         abstract = True
 
 
-class User(AbstractUser, TimestampModelMixin):
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin, TimestampModelMixin):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    is_active = models.BooleanField(default=True)
     email = models.EmailField(unique=True)
     spotify_id = models.CharField(max_length=100)
     spotify_display_name = models.CharField(max_length=100)
@@ -23,3 +44,6 @@ class User(AbstractUser, TimestampModelMixin):
     spotify_account_href = models.URLField(max_length=2000, null=True, blank=True)
     spotify_account_uri = models.CharField(max_length=200, null=True, blank=True)
     spotify_product = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.email
