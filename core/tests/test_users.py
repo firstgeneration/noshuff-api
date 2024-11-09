83d6167ad517271595cfe33d99a337b3d20c42bd
Daniel Gantz <daniel.gantz@gmail.com>
2024-11-09 20:01:22 +0200

    Clean up User API Tests

--- core/tests/test_users.py
+++ core/tests/test_users.py
@@ -1,14 +1,11 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
 from core.tests.factories.user_factory import UserFactory
 
 
-class UserAPITests(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-    
+class UserAPITests(APITestCase):
+
     def test_get_current_user(self):
         current_user = UserFactory()
 
@@ -36,4 +33,3 @@ class UserAPITests(TestCase):
             self.assertEqual(
                 model_value, response_json[field]
             )
-
